6bfafaa0d0f1f4a34a90c0e3d85cca793aee9527
EliteBot47 <elitebot47@gmail.com>
2025-05-01 15:51:41 +0530

    removed some comments

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -37,13 +37,11 @@ export default function Signup() {
       }, 2000);
     } catch (error: any) {
       if (error.response) {
-        // Log server-side error response
         console.error("Error response from server:", error.response.data);
         Seterrors(
           error.response.data.message || "An error occurred during signup."
         );
       } else {
-        // Log network or other types of errors
         console.error("Network or other error:", error);
         Seterrors("Network error or server is down.");
       }
