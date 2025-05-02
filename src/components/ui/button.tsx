5f467db40053cf43bcc57321bd871d5ee73e7972
EliteBot47 <elitebot47@gmail.com>
2025-05-03 02:37:40 +0530

    intialized custom button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export default function SignInButton({
+  classname,
+  variant = "default",
+  size,
+  ...props
+}) {
+
+    return <button>
+        
+    </button>
+}
