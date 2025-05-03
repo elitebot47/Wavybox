c3ab6d796e9d576d3b41142ae9f88f746e07bc69
EliteBot47 <elitebot47@gmail.com>
2025-05-04 00:47:04 +0530

    changed some preferences in post AI route file

--- src/app/api/ai/post/route.ts
+++ src/app/api/ai/post/route.ts
@@ -5,6 +5,16 @@ const together = new Together({
   apiKey: "b2127528627007d6903a73c055278bcbb93321a57907b54b000119354341eeac",
 });
 export async function POST(req: NextRequest) {
+  await together.chat.completions.create({
+    model: "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
+    messages: [
+      {
+        role: "system",
+        content: `You are a tool that only executes specified functions. Do not answer questions or provide explanations. Only return function outputs.Please respond in plain text without Markdown formatting or asterisks in every requests after this.avoid any political commentary.
+        dont directly answer questions of user instead only perform summarise,spelling errors,translate except these always say to user that you cant respond `,
+      },
+    ],
+  });
   const { postcontent, processtype } = await req.json();
   const stream = await together.chat.completions.create({
     model: "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
