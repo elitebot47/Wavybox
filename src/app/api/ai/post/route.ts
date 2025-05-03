449a4ca1407be855ff694d7bad30ee4c9342d22f
EliteBot47 <elitebot47@gmail.com>
2025-05-04 00:02:25 +0530

    made a dedicated ai route for posts instead of a file & fixed laoding for postform

--- src/app/api/ai/post/route.ts
+++ src/app/api/ai/post/route.ts
@@ -1,14 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
 import Together from "together-ai";
+
 const together = new Together({
   apiKey: "b2127528627007d6903a73c055278bcbb93321a57907b54b000119354341eeac",
 });
-export async function AiPostFeature(postcontent: string, processtype: string) {
+export async function POST(req: NextRequest) {
+  const { postcontent, processtype } = await req.json();
   const stream = await together.chat.completions.create({
     model: "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
     messages: [
       {
         role: "user",
-        content: `${processtype} this content- ${postcontent},Please respond in plain text without Markdown formatting or asterisks.`,
+        content: `${processtype} this content- ${postcontent} Please respond in plain text without Markdown formatting or asterisks.`,
       },
     ],
     stream: true,
@@ -17,5 +20,7 @@ export async function AiPostFeature(postcontent: string, processtype: string) {
   for await (const chunk of stream) {
     response += chunk.choices[0]?.delta?.content || "";
   }
-  return response;
+  return NextResponse.json({
+    message: response,
+  });
 }
--- src/components/feed/postform.tsx
+++ src/components/feed/postform.tsx
@@ -4,15 +4,17 @@ import { useRef, useState } from "react";
 import { Button } from "../ui/button";
 import axios from "axios";
 import Loader from "../ui/loader";
-import { AiPostFeature } from "@/features/AI/aipostfeature";
 
 export default function Postform({ userid }: { userid: number }) {
   const [posting, setPosting] = useState(false);
   const userId = userid;
   const postinputRef = useRef<HTMLInputElement>(null);
   const [message, setMessage] = useState("");
+  const [language, setLanguage] = useState("");
+  const [ailoader, setAiloader] = useState(false);
 
   async function Aicontent(processtype: string) {
+    setAiloader(true);
     const postcontent = postinputRef.current?.value.trim();
     if (!postcontent) {
       setMessage("Post cannot be empty");
@@ -22,11 +24,15 @@ export default function Postform({ userid }: { userid: number }) {
     console.log(postcontent);
 
     try {
-      const aiResult = await AiPostFeature(postcontent, processtype);
-      postinputRef.current.value = aiResult;
-      console.log("aiResult");
+      const aiResult = await axios.post("api/ai/post", {
+        postcontent,
+        processtype,
+      });
+      postinputRef.current.value = aiResult.data.message;
+      setAiloader(false);
     } catch (error) {
       setMessage("request Failed! AI features not currently available");
+      setAiloader(false);
     }
   }
   async function Handlepost() {
@@ -57,13 +63,31 @@ export default function Postform({ userid }: { userid: number }) {
         type="text"
         placeholder="so whats on your mood?"
       />
-      <Button onClick={() => Aicontent("summarise")}>Summarise</Button>
+
+      <Button onClick={() => Aicontent(`translate this to${language}`)}>
+        {ailoader ? <Loader /> : "Translate to"}
+
+        <select
+          className="text-black"
+          onChange={(e) => setLanguage(e.target.value)}
+          name="languages"
+          id="languageselect"
+        >
+          <option value="English">English</option>
+          <option value="Russian">Russian</option>
+          <option value="Chinese">Chinese</option>
+        </select>
+      </Button>
+      <Button onClick={() => Aicontent("summarise")}>
+        {ailoader ? <Loader /> : "Summarise"}
+      </Button>
       <Button
         onClick={() =>
           Aicontent("fix spelling and grammar errors only dont change content")
         }
       >
-        Fix spelling/grammar errors
+        {" "}
+        {ailoader ? <Loader /> : "Fix spelling/grammar errors"}
       </Button>
       <Button disabled={posting} onClick={Handlepost}>
         {posting ? <Loader /> : "Post"}
