94299f889f2dcfd8c9f8f7fdc913185d18798270
EliteBot47 <elitebot47@gmail.com>
2025-05-08 03:54:18 +0530

    one line change

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,5 @@
 import PostArea from "@/components/feed/postarea";
 import Postform from "@/components/feed/postform";
-import { Separator } from "@/components/ui/separator";
 import { auth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { Post as PostType } from "@prisma/client";
@@ -24,7 +23,6 @@ export default async function Home() {
       },
     },
   });
-  console.log("posts:", posts);
 
   posts = posts.reverse();
   const userid: number = session.user.id;
