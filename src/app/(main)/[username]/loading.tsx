2b2fa40d6ba7673e6087155b660ce29fc533df68
EliteBot47 <elitebot47@gmail.com>
2025-05-10 21:05:28 +0530

    Minor fixes in navigation & added loader file

---
+++ src/app/(main)/[username]/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Loader from "@/components/ui/loader";
+
+export default function () {
+  return (
+    <div className=" flex justify-center items-center w-full h-full ">
+      <Loader></Loader>
+    </div>
+  );
+}
