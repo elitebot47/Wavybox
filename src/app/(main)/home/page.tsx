122c33f3f220ebbaa21c621a838adbf2537e2f4b
EliteBot47 <elitebot47@gmail.com>
2025-05-11 04:32:38 +0530

    added logs for debugging

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -7,6 +7,8 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
+  console.log("session:" + session);
+
   if (!session) {
     console.log("session was not there");
 
