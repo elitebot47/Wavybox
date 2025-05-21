44a50c5e8757bad86e80da56aced74a51ee2f764
EliteBot47 <elitebot47@gmail.com>
2025-05-21 23:14:13 +0530

    added photoview modal for posts

--- src/components/feed/imagespace.tsx
+++ src/components/feed/imagespace.tsx
@@ -1,21 +1,30 @@
-import { Plus } from "lucide-react";
 import { CldImage } from "next-cloudinary";
 import { useState } from "react";
 interface Image {
   secureUrl: string;
   publicId: string;
 }
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import { Skeleton } from "../ui/skeleton";
 
 interface ImageGridProps {
   images: Image[];
 }
 export default function Imagespace({ images }: ImageGridProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const imageCount = images.length;
 
   const getGridCols = () => {
-    if (imageCount === 1) return "grid-cols-1";
-    if (imageCount === 2) return "grid-cols-2";
+    if (images.length === 1) return "grid-cols-1";
+    if (images.length === 2) return "grid-cols-2";
     return "grid-cols-2";
   };
 
@@ -26,14 +35,29 @@ export default function Imagespace({ images }: ImageGridProps) {
     >
       {images.slice(0, 4).map((image) => (
         <div key={image.publicId} className="relative aspect-[4/3]  ">
-          <CldImage
-            src={image.secureUrl}
-            alt={image.publicId}
-            fill
-            className="object-cover"
-            loading="lazy"
-            onClick={() => setIsOpen(true)}
-          />
+          <Dialog>
+            <DialogTrigger>
+              <CldImage
+                src={image.secureUrl}
+                alt={image.publicId}
+                fill
+                className="object-cover"
+                loading="lazy"
+                onClick={() => setIsOpen(true)}
+              />
+            </DialogTrigger>
+            <DialogTitle></DialogTitle>
+            <DialogContent className="overflow-hidden p-0">
+              <CldImage
+                width={3000}
+                height={3000}
+                src={image.secureUrl}
+                alt={image.publicId}
+                crop="fit"
+                loading="lazy"
+              />
+            </DialogContent>
+          </Dialog>
         </div>
       ))}
     </div>
